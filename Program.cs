9cbcf08c168f6d67f15081a3e9532f13f0eb9504
Nirvik Basnet <nirvik41@gmail.com>
2022-05-11 16:53:41 +1000

    refactor : entity framework setup

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.OpenApi.Models;
-using Bugtracker.DB;
+using Microsoft.EntityFrameworkCore;
+using Bugtracker.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BugDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
   {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+      c.SwaggerDoc("v1", new OpenApiInfo { Title = "BugTrackerAPI", Description = "Keep track of your tasks", Version = "v1" });
   });
 var app = builder.Build();
 
@@ -16,18 +19,17 @@ if(app.Environment.IsDevelopment()){
 app.UseSwagger();
 app.UseSwaggerUI(c =>
   {
-     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+     c.SwaggerEndpoint("/swagger/v1/swagger.json", "BugTrackerAPI V1");
   });
 
 
 //routes
 app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/bug/{id}",(int id) => BugDB.GetBug(id));
-app.MapGet("/bugs",()=>BugDB.GetBugs());
-app.MapPost("/bugs",(Bug bug) => BugDB.CreateBug(bug));
-app.MapPut("/bugs",(Bug bug) => BugDB.UpdateBug(bug));
-app.MapDelete("/bugs/{id}",(int id)=> BugDB.RemoveBug(id));
-
+app.MapGet("/bugs", async(BugDb db)=>await db.Bugs.ToListAsync());
+app.MapPost("/bug",async (BugDb db, Bug bug) =>{
+    await db.Bugs.AddAsync(bug);
+    await db.SaveChangesAsync();
+    return Results.Created($"/bug/{bug.Id}",bug);
+});
 
 app.Run();
